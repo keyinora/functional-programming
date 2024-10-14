9f1d418de942fd2c72b4840b0cec69d26c89cdb7
keyinora <ddraven1989@gmail.com>
2024-10-14 17:11:39 -0400

    added additional sections

---
+++ function_transformations/more_transformations/main.py
@@ -0,0 +1,18 @@
+def doc_format_checker_and_converter(conversion_function, valid_formats):
+    def inner_function(filename, content):
+        if filename.split('.')[1] in valid_formats:
+            return conversion_function(content)
+        raise ValueError("Invalid file format")
+    return inner_function
+
+
+# Don't edit below this line
+
+
+def capitalize_content(content):
+    return content.upper()
+
+
+def reverse_content(content):
+    return content[::-1]
+
